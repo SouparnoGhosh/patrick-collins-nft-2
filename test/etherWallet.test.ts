4ea2345bfc075c239493d09b21fd1a2360fa3831
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-05-19 12:34:13 +0530

    Tested withdraw

--- test/etherWallet.test.ts
+++ test/etherWallet.test.ts
@@ -15,15 +15,17 @@ describe("Ether Wallet Contract Tests", async function () {
     expect(await etherWallet.owner()).to.equal(wallet.address);
   });
 
-  it("Checks the balance which is 10", async function () {
+  it("Checks the balance and withdraw", async function () {
     const provider = new MockProvider();
     const [wallet] = provider.getWallets();
     const etherWallet = await deployContract(wallet, EtherWalletJSON, []);
-
     await etherWallet.take({ value: ethers.utils.parseUnits("10", "ether") });
+
+    await etherWallet.withdraw(ethers.utils.parseUnits("1", "ether"));
+
     const contractBalance = ethers.utils.formatEther(
       await etherWallet.getBalance()
     );
-    expect(contractBalance).to.equal("10.0");
+    expect(contractBalance).to.equal("9.0");
   });
 });
