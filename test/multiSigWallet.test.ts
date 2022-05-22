fb42de723327a8249c74c09e844daf91ec5e110e
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-05-22 09:52:41 +0530

    Move Multisig to diff repo

--- test/multiSigWallet.test.ts
+++ test/multiSigWallet.test.ts
@@ -36,17 +36,6 @@ describe("Testing Multi Sig Wallets", function () {
   }
 
   it("Testing if constructor worked properly", async function () {
-    // const multiSigWallet = await deployContract(msWallet, MultiSigWalletJSON, [
-    //   [
-    //     alice.address,
-    //     bob.address,
-    //     charlie.address,
-    //     dana.address,
-    //     erika.address,
-    //     fawn.address,
-    //   ],
-    //   4,
-    // ]);
     const { multiSigWallet, msWallet, alice, bob, charlie, dana, erika, fawn } =
       await loadFixture(fixture);
 
@@ -74,5 +63,14 @@ describe("Testing Multi Sig Wallets", function () {
     ).to.be.revertedWith("owner present");
   });
 
-  //   it("something", async function () {});
+  it("something", async function () {
+    const { multiSigWallet } = await loadFixture(fixture);
+    const [owner] = await ethers.getSigners();
+    await expect(
+      owner.call({
+        to: multiSigWallet.address,
+        value: ethers.utils.parseEther("1"),
+      })
+    ).to.emit(multiSigWallet, "Deposit");
+  });
 });
